36a951bac4ab3534fdcbd27194e4d9fb2afd26f6
Matt Glover <matthewglover@me.com>
2017-10-02 11:39:45 +0100

    Refactor SimpleController to inhert from ActionController::Metal

--- app/controllers/simple_controller.rb
+++ app/controllers/simple_controller.rb
@@ -1,4 +1,9 @@
-class SimpleController < ActionController::Base
+class SimpleController < ActionController::Metal
+  include AbstractController::Rendering
+  include ActionView::Layouts
+  append_view_path "#{Rails.root}/app/views"
+  
   def index
+    render 'simple/index'
   end
 end
